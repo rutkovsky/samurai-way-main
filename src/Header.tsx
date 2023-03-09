acb895a6b85243bade12ba5aeee49eb085d7c64a
rutkovsky <rus.rutkovsky@yandex.ru>
2023-03-09 20:10:27 +0300

    lesson 10

---
+++ src/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <ul>
+                <li>a</li>
+                <li>b</li>
+            </ul>
+        </div>
+    )
+        ;
+}
+export default Header;
---
+++ src/Technologies.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Technologies = () => {
+    return (
+        <div>
+            Hello, samurai! Let's go!
+            <ul>
+                <li>css</li>
+                <li>html</li>
+            </ul>
+        </div>
+    )
+        ;
+}
+
+export default Technologies;
