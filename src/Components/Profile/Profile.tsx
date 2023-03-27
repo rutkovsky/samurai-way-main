68db69b5627fb68c93e26df7c5ec5e4924dea6b9
rutkovsky <rus.rutkovsky@yandex.ru>
2023-03-27 19:51:51 +0300

    lesson 17

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -8,12 +8,12 @@ const Profile = () => {
         <div className={classes.content}>
             <div className={classes.long}>
                 long pic
-                <img src="https://www.freepngimg.com/thumb/mountains/33431-5-mountains-free-download.png"/>
+                <img alt='Long pic' src="https://www.freepngimg.com/thumb/mountains/33431-5-mountains-free-download.png"/>
             </div>
             <div className={classes.ava}>
                 ava + inf
                 <div>
-                    <img src="https://www.jumping.fitness/wp-content/uploads/2015/07/user.jpg"/>
+                    <img alt='User avatar' src="https://www.jumping.fitness/wp-content/uploads/2015/07/user.jpg"/>
                     Ivan Ivanov
                 </div>
             </div>
